8cdd7d55f1fcc2e9b29c9e8287b76fbf99e2f4a8
unknown <nguyenbaohoa02@gmail.com>
2024-09-23 14:20:14 +0700

    completion main dashboard

--- src/scenes/global/Sidebar.jsx
+++ src/scenes/global/Sidebar.jsx
@@ -1,21 +1,22 @@
 import { useState } from "react";
-import { ProSidebar, Menu, MenuItem } from "react-pro-sidebar";
-import { Box, IconButton, Typography, useTheme } from "@mui/material";
+import { ProSidebar, Menu, MenuItem, SubMenu } from "react-pro-sidebar";
+import { Box, IconButton, Typography, useMediaQuery, useTheme } from "@mui/material";
 import { Link } from "react-router-dom";
 import "react-pro-sidebar/dist/css/styles.css";
 import { tokens } from "../../theme";
 import HomeOutlinedIcon from "@mui/icons-material/HomeOutlined";
-import PeopleOutlinedIcon from "@mui/icons-material/PeopleOutlined";
-import ContactsOutlinedIcon from "@mui/icons-material/ContactsOutlined";
-import ReceiptOutlinedIcon from "@mui/icons-material/ReceiptOutlined";
-import PersonOutlinedIcon from "@mui/icons-material/PersonOutlined";
-import CalendarTodayOutlinedIcon from "@mui/icons-material/CalendarTodayOutlined";
-import HelpOutlineOutlinedIcon from "@mui/icons-material/HelpOutlineOutlined";
 import BarChartOutlinedIcon from "@mui/icons-material/BarChartOutlined";
-import PieChartOutlineOutlinedIcon from "@mui/icons-material/PieChartOutlineOutlined";
 import TimelineOutlinedIcon from "@mui/icons-material/TimelineOutlined";
 import MenuOutlinedIcon from "@mui/icons-material/MenuOutlined";
-import MapOutlinedIcon from "@mui/icons-material/MapOutlined";
+import SettingsRoundedIcon from '@mui/icons-material/SettingsRounded';
+import LocalAtmRoundedIcon from '@mui/icons-material/LocalAtmRounded';
+import HandshakeRoundedIcon from '@mui/icons-material/HandshakeRounded';
+import AccountCircleRoundedIcon from '@mui/icons-material/AccountCircleRounded';
+import AttributionRoundedIcon from '@mui/icons-material/AttributionRounded';
+import AssignmentRoundedIcon from '@mui/icons-material/AssignmentRounded';
+import TroubleshootRoundedIcon from '@mui/icons-material/TroubleshootRounded';
+import RequestQuoteRoundedIcon from '@mui/icons-material/RequestQuoteRounded';
+import LocalShippingRoundedIcon from '@mui/icons-material/LocalShippingRounded';
 
 const Item = ({ title, to, icon, selected, setSelected }) => {
   const theme = useTheme();
@@ -38,8 +39,11 @@ const Item = ({ title, to, icon, selected, setSelected }) => {
 const Sidebar = () => {
   const theme = useTheme();
   const colors = tokens(theme.palette.mode);
+  const isMobile = useMediaQuery("(max-width: 768px)"); // Detect screen size
   const [isCollapsed, setIsCollapsed] = useState(false);
   const [selected, setSelected] = useState("Dashboard");
+  const [isMenuOpen, setIsMenuOpen] = useState(false); // Track menu open/close
+
 
   return (
     <Box
@@ -59,9 +63,21 @@ const Sidebar = () => {
         "& .pro-menu-item.active": {
           color: "#6870fa !important",
         },
+        "& .pro-sub-menu .pro-inner-item": {
+          fontWeight: "bold",
+        },
       }}
     >
-      <ProSidebar collapsed={isCollapsed}>
+      {/* Hamburger Menu Icon */}
+      {isMobile && (
+        <IconButton
+          onClick={() => setIsMenuOpen(!isMenuOpen)}
+          sx={{ position: "fixed", top: 10, left: 10, zIndex: 1000 }}
+        >
+          <MenuOutlinedIcon />
+        </IconButton>
+      )}
+      <ProSidebar collapsed={isCollapsed} toggled={isMenuOpen} breakPoint="md" onToggle={setIsMenuOpen}>
         <Menu iconShape="square">
           {/* LOGO AND MENU ICON */}
           <MenuItem
@@ -73,12 +89,7 @@ const Sidebar = () => {
             }}
           >
             {!isCollapsed && (
-              <Box
-                display="flex"
-                justifyContent="space-between"
-                alignItems="center"
-                ml="15px"
-              >
+              <Box display="flex" justifyContent="space-between" alignItems="center" ml="15px">
                 <Typography variant="h3" color={colors.grey[100]}>
                   ADMINIS
                 </Typography>
@@ -101,123 +112,69 @@ const Sidebar = () => {
                 />
               </Box>
               <Box textAlign="center">
-                <Typography
-                  variant="h2"
-                  color={colors.grey[100]}
-                  fontWeight="bold"
-                  sx={{ m: "10px 0 0 0" }}
-                >
-                  Ed Roh
+                <Typography variant="h2" color={colors.grey[100]} fontWeight="bold" sx={{ m: "10px 0 0 0" }}>
+                  VNG
                 </Typography>
                 <Typography variant="h5" color={colors.greenAccent[500]}>
-                  VP Fancy Admin
+                  VNG Admin
                 </Typography>
               </Box>
             </Box>
           )}
 
           <Box paddingLeft={isCollapsed ? undefined : "10%"}>
-            <Item
-              title="Dashboard"
-              to="/"
-              icon={<HomeOutlinedIcon />}
-              selected={selected}
-              setSelected={setSelected}
-            />
+            <Item title="Dashboard" to="/" icon={<HomeOutlinedIcon />} selected={selected} setSelected={setSelected} />
+            <Item title="Tasks" to="/tasks" icon={<AssignmentRoundedIcon />} selected={selected} setSelected={setSelected} />
+            <SubMenu title="Rates" icon={<TroubleshootRoundedIcon />} sx={{ fontWeight: "bold" }}>
+              <MenuItem icon="FR">
+                <Item title="Freight" to="/freight" selected={selected} setSelected={setSelected} />
+              </MenuItem>
+              <MenuItem icon="LC">
+                <Item title="Local Charges" to="/local-charges" selected={selected} setSelected={setSelected} />
+              </MenuItem>
+              <MenuItem icon="SE">
+                <Item title="Services" to="/services" selected={selected} setSelected={setSelected} />
+              </MenuItem>
+            </SubMenu>
 
-            <Typography
-              variant="h6"
-              color={colors.grey[300]}
-              sx={{ m: "15px 0 5px 20px" }}
-            >
-              Data
-            </Typography>
-            <Item
-              title="Manage Team"
-              to="/team"
-              icon={<PeopleOutlinedIcon />}
-              selected={selected}
-              setSelected={setSelected}
-            />
-            <Item
-              title="Contacts Information"
-              to="/contacts"
-              icon={<ContactsOutlinedIcon />}
-              selected={selected}
-              setSelected={setSelected}
-            />
-            <Item
-              title="Invoices Balances"
-              to="/invoices"
-              icon={<ReceiptOutlinedIcon />}
-              selected={selected}
-              setSelected={setSelected}
-            />
+            <SubMenu title="Quotes" icon={<RequestQuoteRoundedIcon />}>
+              <MenuItem icon="LI">
+                <Item title="List" to="/list" selected={selected} setSelected={setSelected} />
+              </MenuItem>
+              <MenuItem icon="RE">
+                <Item title="Request" to="/request" selected={selected} setSelected={setSelected} />
+              </MenuItem>
+            </SubMenu>
 
-            <Typography
-              variant="h6"
-              color={colors.grey[300]}
-              sx={{ m: "15px 0 5px 20px" }}
-            >
-              Pages
-            </Typography>
-            <Item
-              title="Profile Form"
-              to="/form"
-              icon={<PersonOutlinedIcon />}
-              selected={selected}
-              setSelected={setSelected}
-            />
-            <Item
-              title="Calendar"
-              to="/calendar"
-              icon={<CalendarTodayOutlinedIcon />}
-              selected={selected}
-              setSelected={setSelected}
-            />
-            <Item
-              title="FAQ Page"
-              to="/faq"
-              icon={<HelpOutlineOutlinedIcon />}
-              selected={selected}
-              setSelected={setSelected}
-            />
+            <SubMenu title="Shipments" icon={<LocalShippingRoundedIcon />}>
+              <MenuItem icon="LI">
+                <Item title="List" to="/list" selected={selected} setSelected={setSelected} />
+              </MenuItem>
+              <MenuItem icon="MA">
+                <Item title="Manifest" to="/manifest" selected={selected} setSelected={setSelected} />
+              </MenuItem>
+              <MenuItem icon="CU">
+                <Item title="Customs" to="/customs" selected={selected} setSelected={setSelected} />
+              </MenuItem>
+              <MenuItem icon="TR">
+                <Item title="Trucking" to="/trucking" selected={selected} setSelected={setSelected} />
+              </MenuItem>
+              <MenuItem icon="CO">
+                <Item title="Consol" to="/consol" selected={selected} setSelected={setSelected} />
+              </MenuItem>
+            </SubMenu>
 
-            <Typography
-              variant="h6"
-              color={colors.grey[300]}
-              sx={{ m: "15px 0 5px 20px" }}
-            >
-              Charts
-            </Typography>
-            <Item
-              title="Bar Chart"
-              to="/bar"
-              icon={<BarChartOutlinedIcon />}
-              selected={selected}
-              setSelected={setSelected}
-            />
-            <Item
-              title="Pie Chart"
-              to="/pie"
-              icon={<PieChartOutlineOutlinedIcon />}
-              selected={selected}
-              setSelected={setSelected}
-            />
-            <Item
-              title="Line Chart"
-              to="/line"
-              icon={<TimelineOutlinedIcon />}
-              selected={selected}
-              setSelected={setSelected}
-            />
-            <Item
-              title="Geography Chart"
-              to="/geography"
-              icon={<MapOutlinedIcon />}
-              selected={selected}
-              setSelected={setSelected}
-            />
+            <hr style={{ borderColor: colors.grey[300], margin: "10px 0" }} />
+            <Item title="Leads" to="/leads" icon={<AccountCircleRoundedIcon />} selected={selected} setSelected={setSelected} />
+            <Item title="Clients" to="/clients" icon={<AttributionRoundedIcon />} selected={selected} setSelected={setSelected} />
+            <Item title="Providers" to="/providers" icon={<HandshakeRoundedIcon />} selected={selected} setSelected={setSelected} />
+
+            <hr style={{ borderColor: colors.grey[300], margin: "10px 0" }} />
+            <Item title="Accounting" to="/accounting" icon={<LocalAtmRoundedIcon />} selected={selected} setSelected={setSelected} />
+            <Item title="Reports" to="/reports" icon={<TimelineOutlinedIcon />} selected={selected} setSelected={setSelected} />
+
+            <hr style={{ borderColor: colors.grey[300], margin: "10px 0" }} />
+            <Item title="Setting" to="/setting" icon={<SettingsRoundedIcon />} selected={selected} setSelected={setSelected} />
           </Box>
         </Menu>
       </ProSidebar>
